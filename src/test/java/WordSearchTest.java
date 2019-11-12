aede6913bc1b93f55fe89afc60c89054cd215f3b
Glen Worsley <glen.worsley@shinesolutions.com>
2019-11-12 18:55:23 +1100

    refactor test

--- src/test/java/WordSearchTest.java
+++ src/test/java/WordSearchTest.java
@@ -4,95 +4,47 @@ import static org.junit.Assert.*;
 
 public class WordSearchTest {
 
+    private static final WordSearch WORD_SEARCH = new WordSearch();
+    private static final String[][] MATRIX = new String[][]{
+            {"F", "A", "C", "I"},
+            {"O", "B", "Q", "P"},
+            {"A", "N", "O", "B"},
+            {"M", "A", "S", "S"}
+    };
+
     @Test
     public void testVerticalWordInCol1() {
-        WordSearch wordSearch = new WordSearch();
-        String[][] matrix = {
-                { "F", "A", "C", "I" },
-                { "O", "B", "Q", "P" },
-                { "A", "N", "O", "B" },
-                { "M", "A", "S", "S" }
-        };
-        String targetWord = "FOAM";
-        assertTrue(wordSearch.search(matrix, targetWord));
+        assertTrue(WORD_SEARCH.search(MATRIX, "FOAM"));
     }
 
     @Test
     public void testWordNotFound() {
-        WordSearch wordSearch = new WordSearch();
-        String[][] matrix = {
-                { "F", "A", "C", "I" },
-                { "O", "B", "Q", "P" },
-                { "A", "N", "O", "B" },
-                { "M", "A", "S", "S" }
-        };
-        String targetWord = "SOSO";
-        assertFalse(wordSearch.search(matrix, targetWord));
+        assertFalse(WORD_SEARCH.search(MATRIX, "SOSO"));
     }
 
     @Test
     public void testHorizontalWordInRow1() {
-        WordSearch wordSearch = new WordSearch();
-        String[][] matrix = {
-                { "F", "A", "C", "I" },
-                { "O", "B", "Q", "P" },
-                { "A", "N", "O", "B" },
-                { "M", "A", "S", "S" }
-        };
-        String targetWord = "FACI";
-        assertTrue(wordSearch.search(matrix, targetWord));
+        assertTrue(WORD_SEARCH.search(MATRIX, "FACI"));
     }
 
     @Test
     public void testHorizontalWordInRow2() {
-        WordSearch wordSearch = new WordSearch();
-        String[][] matrix = {
-                { "F", "A", "C", "I" },
-                { "O", "B", "Q", "P" },
-                { "A", "N", "O", "B" },
-                { "M", "A", "S", "S" }
-        };
-        String targetWord = "OBQP";
-        assertTrue(wordSearch.search(matrix, targetWord));
+        assertTrue(WORD_SEARCH.search(MATRIX, "OBQP"));
     }
 
 
     @Test
     public void testVerticalWordInCol2() {
-        WordSearch wordSearch = new WordSearch();
-        String[][] matrix = {
-                { "F", "A", "C", "I" },
-                { "O", "B", "Q", "P" },
-                { "A", "N", "O", "B" },
-                { "M", "A", "S", "S" }
-        };
-        String targetWord = "ABNA";
-        assertTrue(wordSearch.search(matrix, targetWord));
+        assertTrue(WORD_SEARCH.search(MATRIX, "ABNA"));
     }
 
     @Test
     public void testVerticalWordInCol4() {
-        WordSearch wordSearch = new WordSearch();
-        String[][] matrix = {
-                { "F", "A", "C", "I" },
-                { "O", "B", "Q", "P" },
-                { "A", "N", "O", "B" },
-                { "M", "A", "S", "S" }
-        };
-        String targetWord = "IPBS";
-        assertTrue(wordSearch.search(matrix, targetWord));
+        assertTrue(WORD_SEARCH.search(MATRIX, "IPBS"));
     }
 
     @Test
     public void testHorizontalWordInRow4() {
-        WordSearch wordSearch = new WordSearch();
-        String[][] matrix = {
-                { "F", "A", "C", "I" },
-                { "O", "B", "Q", "P" },
-                { "A", "N", "O", "B" },
-                { "M", "A", "S", "S" }
-        };
-        String targetWord = "MASS";
-        assertTrue(wordSearch.search(matrix, targetWord));
+        assertTrue(WORD_SEARCH.search(MATRIX, "MASS"));
     }
 }
